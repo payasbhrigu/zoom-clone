e7b4b5b84eeef37dcfb6909add11f40522b50f38
Payas Bhrigu <bhrigu.payas10@gmail.com>
2022-04-11 16:03:45 +0530

    Peer to peer

--- public/script.js
+++ public/script.js
@@ -1,4 +1,13 @@
+const socket = io('/')
+const videoGrid = document.getElementById('video-grid')
 const myVideo = document.createElement('video')
+myVideo.muted = true
+
+var peer = new Peer(undefined,{
+    path: '/peerjs',
+    host: '/',
+    port: '3030'
+})
 
 let myVideoStream
 navigator.mediaDevices.getUserMedia({
@@ -9,9 +18,23 @@ navigator.mediaDevices.getUserMedia({
     addVideoStream(myVideo,stream); 
 } )
 
+peer.on('open', id => {
+    console.log("Id: ",id)
+    socket.emit('join-room',ROOM_ID, id);
+})
+
+socket.on('user-connected',() => {
+    connectToNewUser();
+} )
+
+const connectToNewUser = () => {
+    console.log('new user');
+}
+
 const addVideoStream = (video, stream)=>{
     video.srcObject = stream;
     video.addEventListener('loadedmetadata',()=>{
-
+        video.play();
     })
+    videoGrid.append(video);
 }
